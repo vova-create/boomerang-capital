042604bb39e2072028a360c231f435b4bf0d72e7
oleniev_vova <oleniev9@gmail.com>
2023-07-18 18:10:36 +0300

    my second commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <div id="app">
+  <div>
   <TheHeader/>
     <DynamicMarketResponseBlock/>
+    <MarketInfoBlock/>
   </div>
 </template>
 
 <script>
 import TheHeader from "@/components/TheHeader";
 import DynamicMarketResponseBlock from "@/components/DynamicMarketResponseBlock";
+import MarketInfoBlock from "@/components/MarketInfoBlock";
 
 export default {
   name: 'App',
   components: {
     TheHeader,
     DynamicMarketResponseBlock,
+    MarketInfoBlock
   }
 }
 </script>
@@ -22,14 +25,21 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap')
 *
   box-sizing: border-box
+  font-family: Poppins, sans-serif
+  margin: 0
+  padding: 0
 
 #app
-  background-color: var(--main-bg-color)
   min-height: 100vh
   color: var(--main-text-color)
+  width: 100%
+  margin: 0 auto
+  max-width: 1464px
+  padding: 0 20px
+  position: relative
+
 body
-  margin: 0
-  font-family: Poppins, sans-serif
+  background-color: var(--main-bg-color)
 
 img
   object-fit: contain
--- src/components/MarketInfoBlock.vue
+++ src/components/MarketInfoBlock.vue
@@ -1,5 +1,26 @@
 <template>
-
+<div class="market-info-block">
+  <div class="market-info-block__item">
+    <div class="market-info-block__item-name">Holders</div>
+    <div class="market-info-block__item-number">1,601,211</div>
+  </div>
+  <div class="market-info-block__item">
+    <div class="market-info-block__item-name">Market Cap</div>
+    <div class="market-info-block__item-number">$4,389,354.24</div>
+  </div>
+  <div class="market-info-block__item">
+    <div class="market-info-block__item-name">Circulating supply</div>
+    <div class="market-info-block__item-number">$24,125,624</div>
+  </div>
+  <div class="market-info-block__item">
+    <div class="market-info-block__item-name">Price</div>
+    <div class="market-info-block__item-number">$13,114</div>
+  </div>
+  <div class="market-info-block__item">
+    <div class="market-info-block__item-name">Backing</div>
+    <div class="market-info-block__item-number">$2,985</div>
+  </div>
+</div>
 </template>
 
 <script>
